666e21c4b1af7f31efddd025a16c9723e44157a3
alber <alberto@visualnumis.com>
2019-09-04 10:00:14 +0200

    Afegim Hola Mon

---
+++ src/clsHolaMon.java
@@ -0,0 +1,14 @@
+import java.util.logging.Logger;
+
+public class clsHolaMon {
+	private static Logger logger = Logger.getLogger("clsHolaMon.log");
+	private static String getVersion() {
+		return "1.0";
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		logger.info("· Starting UserService.main(String[] args)" + getVersion());
+
+	}
+
+}
